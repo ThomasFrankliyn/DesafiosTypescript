9b25b842d8d1039d3c9e05a60474effbb0df3f7c
Thomas <thomasfrankliyn@gmail.com>
2022-06-30 11:25:34 -0300

    Desafio DIO

---
+++ Desafios/Desafio1.js
@@ -0,0 +1,26 @@
+"use strict";
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+let employee = {
+    code: 10,
+    name: "John"
+};
+employee.code = 10;
+employee.name = "John";
+// Resposta 1
+const funcionario = {
+    codigo: 10,
+    nome: 'João'
+};
+// Resposta 2
+const funcionario2 = {
+    codigo: 10,
+    nome: 'joao'
+};
+;
+const funcionarioObj = {};
+funcionarioObj.codigo = 10;
+funcionarioObj.nome = 'João';
+const funcionarioObj2 = {
+    codigo: 10,
+    nome: 'João'
+};
